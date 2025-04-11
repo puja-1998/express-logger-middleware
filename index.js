d43c91f9b1362a0bd1a2a8bcb52f5baa0651efa5
Puja-1998 <ghodkepooja98@gmail.com>
2025-04-11 15:00:13 +0530

    Added console output

--- index.js
+++ index.js
@@ -4,10 +4,34 @@ const app = express();
 
 const port = 3000;
 
+// middleware
+
+const loggerMiddleware = (req,res,next) =>{
+    const start = Date.now();
+    const timestamp = new Date().toISOString();
+    const Name = "Puja";
+    
+    res.on('finish', () =>{
+        const duration = Date.now() - start;
+        console.log(`My Name is: ${Name}`);
+        console.log(`Timestam:- ${timestamp}`);
+        console.log(`duration:- ${duration}ms`);
+        console.log(`Ip Address:- ${req.ip}`);
+        console.log(`Methods:- ${req.method}`);
+        console.log(`Original URl:- ${req.originalurl}`);
+        console.log(`Status Code:- ${req.statusCode}`);
+    });
+
+    next();
+};
+
+// using middleware
+app.use(loggerMiddleware);
+
 app.get('/', (req, res) =>{
-    res.send("Hello Puja, I am middleware");
+    res.send("Hello Puja, I am middleware!");
 });
 
-app.listen('port', () =>{
+app.listen(port, () =>{
     console.log(`server is running on the ${port}`);
 });
